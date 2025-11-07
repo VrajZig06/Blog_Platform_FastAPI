e016cb8588317ffdc551ca5032bc03a4761cc78e
Vraj Makwana <vrajm@zignuts.com>
2025-11-07 12:53:50 +0530

    Updt: Add User Table

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,3 +1,22 @@
 from app.db.base import Base
-from sqlalchemy import Integer,Column
+from sqlalchemy import Integer,Column,String,Text,Enum
+from enum import Enum as PyEnum
+import uuid
 
+class UserType(str,PyEnum):
+    ADMIN = "admin"
+    AUTHOR = "author"
+    READER = "reader"
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(String,default=lambda:str(uuid.uuid4()),primary_key=True,nullable=False)
+    first_name = Column(String,nullable=False)
+    last_name = Column(String,nullable=False)
+    email = Column(String,nullable=False,unique=True)
+    password = Column(String,nullable=False)
+    age = Column(Integer,nullable=False)
+    gender = Column(String,nullable=False)
+    profile_photo = Column(Text,nullable=True)  
+    user_type = Column(Enum(UserType), nullable=False, default=UserType.READER)
--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
 
 from fastapi import FastAPI
 
-from app.db import models,session,base
+from app.db import session,base,models
 
 # Create Fast API App
 app = FastAPI()
